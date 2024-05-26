79b9d02805a4e39922f3aee49566921799383e37
hotardaw <hotardaw@gmail.com>
2024-05-26 09:49:42 -0500

    Added authentication

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,4 +1,104 @@
+import { useRef, useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
+import { setCredentials } from './authSlice'
+import { useLoginMutation } from './authApiSlice'
+
 const Login = () => {
-  return <h1>Login</h1>
+  const userRef = useRef()
+  const errRef = useRef()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errMsg, setErrMsg] = useState('')
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  // 'isLoading' is a state from useLoginMutation
+  const [login, { isLoading }] = useLoginMutation()
+
+  // When component mounts, focus on 'username' field in DOM
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  // Clear the error message state when username/password state changes
+  useEffect(() => {
+    setErrMsg('')
+  }, [username, password])
+
+  const handleUserInput = (e) => setUsername(e.target.value)
+  const handlePwdInput = (e) => setPassword(e.target.value)
+
+  const handleSubmit = async (e) => {
+    // Default is to reload the page on submit, so:
+    e.preventDefault()
+    try {
+      // unwrap() removes the parent HTML element
+      const { accessToken } = await login({ username, password }).unwrap()
+      dispatch(setCredentials({ accessToken }))
+      setUsername('')
+      setPassword('')
+      navigate('/dash')
+    } catch (err) {
+      if (!err.status) {
+        setErrMsg('No Server Response')
+      } else if (err.status === 400) {
+        setErrMsg('Missing Username or Password')
+      } else if (err.status === 401) {
+        setErrMsg('Unauthorized')
+      } else {
+        setErrMsg(err.data?.message)
+      }
+      errRef.current.focus() // set focus to err msg
+    }
+  }
+
+  const errClass = errMsg ? 'errmsg' : 'offscreen'
+
+  if (isLoading) return <p>Loading...</p>
+
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Employee Login</h1>
+      </header>
+      <main className="login">
+        <p ref={errRef} className={errClass} aria-live="assertive">
+          {errMsg}
+        </p>
+
+        <form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            className="form__input"
+            type="text"
+            id="username"
+            ref={userRef}
+            value={username}
+            onChange={handleUserInput}
+            autoComplete="off"
+            required
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            className="form__input"
+            type="password"
+            id="password"
+            onChange={handlePwdInput}
+            value={password}
+            required
+          />
+          <button className="form__submit-button">Sign In</button>
+        </form>
+      </main>
+      <footer>
+        <Link to="/">Back to Home</Link>
+      </footer>
+    </section>
+  )
+
+  return content
 }
 export default Login
